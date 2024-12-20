32129cde441bf579c4272e123a42e275f12dec41
ckish3 <ckish1@gmail.com>
2024-12-20 08:59:19 -0800

    Add game simulate agent tool

--- nfl_predict/nfl_trueskill.py
+++ nfl_predict/nfl_trueskill.py
@@ -5,6 +5,37 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 import trueskill
+from transformers import tool
+
+
+DF_ALL_TRUESKILLS = None # The dataframe of all trueskill ratings. It would be much better if this wasn't
+    # a global variable, but the HuggingFace agent library doesn't allow tool inputs to be dataframes, 
+    # so it has to be a global variable
+
+def get_latest_ratings(df_all_trueskills: pd.DataFrame) -> dict:
+    """
+    Returns a dictionary of the latest trueskill ratings (the values for the latest week 
+    in the latest season)
+
+    Args:
+        df_all_trueskills (pd.DataFrame): The dataframe of all trueskill ratings
+
+    Returns:
+        dict: the latest trueskill ratings for just the latest season
+    """
+    latest_ratings = {}
+
+    latest_season = max(df_all_trueskills['season'].values)
+    season_df = df_all_trueskills[df_all_trueskills['season'] == latest_season]
+
+    for team, team_df in season_df.groupby('team'):
+        latest_week = max(team_df['week'].values)
+        latest_df = team_df[team_df['week'] == latest_week]
+
+        latest_ratings[team] = {'mu': latest_df['trueskill_rating'], 
+                                'sigma': latest_df['trueskill_sigma']}
+
+    return latest_ratings
 
 
 def trueskill_test_score(row, home_field_advantage):
@@ -100,7 +131,7 @@ def trueskill_rate(df_games: pd.DataFrame, teams: np.ndarray, home_field_advanta
                 season_trueskill_ratings[loser] = trueskill.Rating(mu=season_trueskill_ratings[loser].mu + (r_loser.mu - r_loser_before.mu),
                                                                     sigma=r_loser.sigma)
                         
-            all_trueskills['season'].append(row['season')
+            all_trueskills['season'].append(row['season'])
             all_trueskills['week'].append(row['week'])
             all_trueskills['team'].append(winner)
             all_trueskills['trueskill_rating'].append(season_trueskill_ratings[winner].mu)
@@ -129,26 +160,24 @@ def evaluate_trueskill(df_training, df_val, teams, beta, home_field_advantage, t
     df_all_trueskills, trueskills_df = trueskill_rate(df_training, teams, home_field_advantage=home_field_advantage, beta=beta, tau=tau)
     return trueskill_metric(df_val, trueskills_df, home_field_advantage)
 
-
-def simulate_game_result(home_team: str, away_team: str, home_field_advantage: int, trueskill_df: pd.DataFrame) -> float:
+@tool
+def simulate_game_result(home_team: str, away_team: str, home_field_advantage: int) -> float:
     """
     Returns the probability that the home team will win the game against the away team given the home field advantage that
     the home team has and ELO ratings given by the pandas dataframe elos_df.
     
     Args:
-        home_team (str): The home team in the game
-        away_team (str): The away team in the game
-        home_field_advantage (int): The home field advantage for the Elo rating
-        trueskill_df (pd.DataFrame): A dataframe of trueskill ratings for just the latest season
+        home_team: (str) The home team in the game
+        away_team: (str) The away team in the game
+        home_field_advantage: (int) The home field advantage for the Trueskill rating
+        trueskill_df: (pd.DataFrame) A dataframe of trueskill ratings for just the latest season
 
     Returns:
         float: The probability that the home team will win the game
     """
+    latest_ratings = get_latest_ratings(DF_ALL_TRUESKILLS)
+    mu_combined = latest_ratings[home_team]['mu'] + home_field_advantage - latest_ratings[away_team]['mu']
 
-    mu_combined = trueskill_df[trueskill_df['team'] == home_team]['trueskill_rating'].values[0] + home_field_advantage - \
-        trueskill_df[trueskill_df['team'] == away_team]['trueskill_rating'].values[0]
-
-    sigma = sqrt(trueskill_df[trueskill_df['team'] == home_team]['trueskill_sigma'].values[0]**2 + trueskill_df[trueskill_df['team'] == away_team]['trueskill_sigma'].values[0]**2)
+    sigma = sqrt(latest_ratings[home_team]['sigma']**2 + latest_ratings[away_team]['sigma']**2)
 
     return 1 - stats.norm.cdf(0, loc=mu_combined, scale=sigma)
-    
